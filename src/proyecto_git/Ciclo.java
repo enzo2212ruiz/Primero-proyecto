68eb901811385a5dc4b9b6b89d8f6b164d31d886
enzo2212ruiz <enzo2212ruiz@gmail.com>
2025-11-23 23:14:11 +0100

    Primer commit
    Primer commit con la clase ciclo.java

---
+++ src/proyecto_git/Ciclo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package proyecto_git;
+
+/**
+ * 
+ */
+public class Ciclo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/proyecto_git/Nombre.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package proyecto_git;
+
+/**
+ * 
+ */
+public class Nombre {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Enzo Ruiz Ráez");
+
+	}
+
+}
